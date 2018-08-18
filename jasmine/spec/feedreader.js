d432092717431f2c3bb72432c7736c5de1003eda
Patrick Woodburne <p.wood@live.ca>
2018-08-18 15:41:10 -0400

    Feat: adjust beforeEach function to pre-load a feed, use new test to load a second feed and make sure the two do not match

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,7 +83,7 @@ describe('The menu', function() {
 });
     /* Completed: Write a new test suite named "Initial Entries" */
 describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Completed: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -102,15 +102,26 @@ describe('Initial Entries', function() {
 });
     /* Completed: Write a new test suite named "New Feed Selection" */
 describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Completed: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         var startFeed;
+         var endFeed;
+
          beforeEach(function(done){
            loadFeed(0, function(){
+             startFeed = $('.feed').html();
              done();
            });
          });
 
-});
+         it('changes feed content after load', function(done){
+           loadFeed(1, function(){
+             endFeed = $('.feed').html();
+             expect(endFeed).not.toEqual(startFeed);
+             done();
+           });
+         });
+       });
 }());
